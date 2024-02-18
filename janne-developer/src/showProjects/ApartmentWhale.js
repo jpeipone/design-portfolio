0710a94a8494b10f6b1c28c0472ca687363abfdd
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2024-02-18 18:29:24 +0200

    react and parallax scrolling and banner

---
+++ janne-developer/src/showProjects/ApartmentWhale.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import "./ApartmentWhale.css";
+import { motion } from "framer-motion";
+
+const ApartmentWhale = ({ images, upOrDown, yScroll, initialY, speed }) => {
+  const [imagesA, setImagesA] = useState(images);
+
+  return (
+    <div className="parallax-apartment-container">
+      {imagesA.map((image) => (
+        <motion.img
+          key={image?.id}
+          src={`/images/${image?.name}`}
+          alt={image?.name}
+          className="img-apartment"
+          initial={{
+            y: upOrDown === 1 ? "-50vh" : 0,
+          }}
+          animate={{ opacity: 1, y: upOrDown * (yScroll * speed) }}
+          transition={{ delay: 0.1 }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ApartmentWhale;
